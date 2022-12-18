414a1f54a9d197b9021f218e72dbea25ab99625b
rusty-gahoi <vipulrocks28@gmail.com>
2022-12-18 18:34:43 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
 	return (
 		<div id="main">
 			{
-				(displayMsg == false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>)
+				(displayMsg == false)?(null):(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
 			}
 			<button id="click" onClick={()=>setDisplayMsg(true)}>Click me</button>
 		</div>
